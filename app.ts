63ed38e2d534ab138fc8f2906108e9e07a1690b0
rob kerr <reallyrobkerr@gmail.com>
2024-06-24 22:51:27 -0700

    refactor updateQuality function into separate update functions using a new item type property

--- app.ts
+++ app.ts
@@ -7,87 +7,87 @@ export class StoreFront {
 
   updateQuality(): void {
     for (let item of this.items) {
-      // not brie or backstage pass
-      if (item.name !== "Aged Brie" && item.name !== "VIP Pass: Concert A-123-F") {
-        // qual is more than 0
-        if (item.quality > 0) {
-          // anything other than gScim or CMC, degrade by 1
-          if (item.name !== "Golden Scimitar" && item.name !== "Conjured Mana Cake") {
-            item.quality--;
-          }
-
-          //"Conjured" items degrade in Quality twice as fast as normal items.
-          if (item.name === "Conjured Mana Cake") {
-            item.quality -= 2;
-          }
-        }
+      if (item.type === "aged" || item.type === "special") {
+        this.updateSpecialItemQuality(item);
       } else {
-        // is brie or backstage pass
-        if (item.quality < 50) {
-          // bump qual by 1 for brie and backstage pass
-          // this is the first increment of 1
-          item.quality++;
-          if (item.name === "VIP Pass: Concert A-123-F") {
-            // BSP: less than 10 days, bump qual by an additional 1
-            if (item.sellDays < 11) {
-              if (item.quality < 50) {
-                item.quality++;
-              }
-            }
-            // BSP: less than 5 days, bump qual by yet another 1 (including the +1 bump from above, total 3)
-            if (item.sellDays < 6) {
-              if (item.quality < 50) {
-                item.quality++;
-              }
-            }
-          }
-        }
+        this.updateCommonItemQuality(item);
       }
 
-      // g.scim doesn't degrade
-      if (item.name !== "Golden Scimitar") {
+      if (item.type !== "legendary") {
         item.sellDays--;
       }
 
-      // expired items
       if (item.sellDays < 0) {
-        if (item.name !== "Aged Brie") {
-          if (item.name !== "VIP Pass: Concert A-123-F") {
-            if (item.quality > 0) {
-              if (item.name !== "Golden Scimitar") {
-                if (item.name === "Conjured Mana Cake") {
-                  item.quality -= 2;
-                } else {
-                  item.quality--;
-                }
-              }
-            }
-          } else {
-            item.quality = 0;
-          }
-        } else {
-          if (item.quality < 50) {
-            // this is the second increment of 1 for expired items
-            item.quality++;
-          }
+        this.updateExpiredItemQuality(item);
+      }
+    }
+  }
+
+  updateCommonItemQuality(item: Item): void {
+    if (item.quality > 0 && item.name !== "Golden Scimitar") {
+      if (item.name === "Conjured Mana Cake") {
+        item.quality -= 2;
+      } else {
+        item.quality--;
+      }
+    }
+  }
+
+  updateSpecialItemQuality(item: Item): void {
+    if (item.quality < 50) {
+      item.quality++;
+      if (item.type === "special") {
+        if (item.sellDays < 11) {
+          item.quality++;
+        }
+
+        if (item.sellDays < 6) {
+          item.quality++;
         }
       }
+    }
+  }
+
+  updateExpiredItemQuality(item: Item): void {
+    switch (item.type) {
+      case "aged":
+        if (item.quality < 50) {
+          item.quality++;
+        }
+        break;
+
+      case "special":
+        item.quality = 0;
+        break;
+
+      case "legendary":
+        break;
+
+      case "conjured":
+        item.quality -= 2;
+        break;
+
+      default:
+        item.quality--;
+        break;
     }
   }
 }
 
 export class Item {
   name: string;
+  type: string;
   quality: number;
   sellDays: number;
 
-  constructor(name: string, sellDays: number, quality: number) {
+  constructor(name: string, type: string, quality: number, sellDays: number) {
     this.name = name;
+    this.type = type;
     this.quality = quality;
     this.sellDays = sellDays;
   }
 
   toString(): string {
-    return `${this.name}, ${this.sellDays}, ${this.quality}`;
+    return `${this.name}, ${this.type}, ${this.quality}, ${this.sellDays}`;
   }
 }
