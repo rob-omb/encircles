69c71665e5a8b7cc679b3abe75accd86a3824f16
rob kerr <reallyrobkerr@gmail.com>
2024-06-24 22:51:57 -0700

    update store items to include item type property

--- index.ts
+++ index.ts
@@ -2,22 +2,22 @@ import { Item, StoreFront } from "./app";
 
 // Run the StoreFront for a number of days
 const items: Item[] = [
-  new Item("+5 Dexterity Vest", 10, 20),
-  new Item("Aged Brie", 2, 0),
-  new Item("Elixir of the Mongoose", 5, 7),
-  new Item("Golden Scimitar", 0, 80),
-  new Item("Golden Scimitar", -1, 80),
-  new Item("VIP Pass: Concert A-123-F", 15, 20),
-  new Item("VIP Pass: Concert A-123-F", 10, 20),
-  new Item("VIP Pass: Concert A-123-F", 5, 20),
-  new Item("Conjured Mana Cake", 3, 6),
-  new Item("Conjured Mana Cake", 3, 60),
+  new Item("+5 Dexterity Vest", "common", 20, 10),
+  new Item("Aged Brie", "aged", 0, 2),
+  new Item("Elixir of the Mongoose", "common", 7, 5),
+  new Item("Golden Scimitar", "legendary", 80, 0),
+  new Item("Golden Scimitar", "legendary", 80, -1),
+  new Item("VIP Pass: Concert A-123-F", "special", 20, 15),
+  new Item("VIP Pass: Concert A-123-F", "special", 20, 10),
+  new Item("VIP Pass: Concert A-123-F", "special", 20, 5),
+  new Item("Conjured Mana Cake", "conjured", 6, 3),
+  new Item("Conjured Mana Cake", "conjured", 50, 3),
 ];
 const days: number = 20;
 
 for (let day: number = 0; day < days; day++) {
   console.log(`-------- day ${day} --------`);
-  console.log("name, sellIn, quality");
+  console.log("name, type, quality, sellDays");
   for (const item of items) {
     console.log(item.toString());
   }
