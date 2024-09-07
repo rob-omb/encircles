3b20095991a26ccdd066b3b7c7f0db2781ca3407
rob kerr <reallyrobkerr@gmail.com>
2024-09-07 01:04:09 -0700

    use contants, add TODO note

--- index.ts
+++ index.ts
@@ -1,17 +1,23 @@
-import { Item, StoreFront } from "./app";
+import { productType } from "./constants";
+import { Item, StoreFront, LegendaryItem } from "./app";
+
+const { normal, agedBrie, conjured, legendary, backstagePass } = productType;
 
 // Run the StoreFront for a number of days
+/**
+ * TODO: finish refactoring so that legendary items have a preset quality that can't be altered
+ */
 const items: Item[] = [
-  new Item("+5 Dexterity Vest", "common", 20, 10),
-  new Item("Aged Brie", "aged", 0, 2),
-  new Item("Elixir of the Mongoose", "common", 7, 5),
-  new Item("Golden Scimitar", "legendary", 80, 0),
-  new Item("Golden Scimitar", "legendary", 80, -1),
-  new Item("VIP Pass: Concert A-123-F", "special", 20, 15),
-  new Item("VIP Pass: Concert A-123-F", "special", 20, 10),
-  new Item("VIP Pass: Concert A-123-F", "special", 20, 5),
-  new Item("Conjured Mana Cake", "conjured", 6, 3),
-  new Item("Conjured Mana Cake", "conjured", 50, 3),
+  new Item("+5 Dexterity Vest", normal, 20, 10),
+  new Item("Aged Brie", agedBrie, 0, 2),
+  new Item("Elixir of the Mongoose", normal, 7, 5),
+  // new Item("Golden Scimitar", legendary),
+  new Item("Golden Scimitar", legendary, 80, -1),
+  new Item("VIP Pass: Concert A-123-F", backstagePass, 20, 15),
+  new Item("VIP Pass: Concert A-123-F", backstagePass, 20, 10),
+  new Item("VIP Pass: Concert A-123-F", backstagePass, 20, 5),
+  new Item("Conjured Mana Cake", conjured, 6, 3),
+  new Item("Conjured Mana Cake", conjured, 50, 3),
 ];
 const days: number = 20;
 
